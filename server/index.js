fabc6f6cd64b61c016ff7f06399e3a846a8e5d77
simonlimon <simon.posada29@gmail.com>
2017-03-13 00:33:08 -0500

    Clean up

--- server/index.js
+++ server/index.js
@@ -9,7 +9,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
-
+app.get('/api/epic_image', function (req, res) {
+  res.send('Hello World!')
+})
 
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
