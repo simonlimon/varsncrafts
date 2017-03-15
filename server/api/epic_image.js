ab086ad1a2a3de3cc6f82d5f274140313bd4c407
simonlimon <simon.posada29@gmail.com>
2017-03-14 23:09:25 -0500

    Better image time selection

--- server/api/epic_image.js
+++ server/api/epic_image.js
@@ -6,17 +6,20 @@ const uid = require('uid');
 
 function get(res, only_meta) {
   axios.get('https://epic.gsfc.nasa.gov/api/natural').then(function (result) {
+
+    var index = Math.floor((result.data.length / 24) * new Date().getHours())
+
     if (only_meta){
       res.setHeader('Content-Type', 'application/json');
-      res.send(result.data[result.data.length-1])
+      res.send(result.data[index])
       return
     }
 
-    var date = result.data[result.data.length-1].date.split(' ')[0].split('-');
+    var date = result.data[index].date.split(' ')[0].split('-');
     var image_link = "https://epic.gsfc.nasa.gov/archive/natural/"
       + date[0] + '/' + date[1] + '/' + date[2] +
       "/png/" + result.data[0].image +'.png';
-    var filepath = __dirname + '/tmp/' + result.data[result.data.length-1].image + '.png';
+    var filepath = __dirname + '/tmp/' + result.data[index].image + '.png';
 
     if (!fs.existsSync(__dirname + '/tmp')){
       fs.mkdirSync(__dirname + '/tmp');
