cf32bd4fa2ad623b966bbbc6d50cfcb38a6ecdd8
simonlimon <simon.posada29@gmail.com>
2017-05-07 14:43:08 -0400

    New craft: perlin walks

---
+++ react-ui/src/crafts/Perlin Walks/Main.js
@@ -0,0 +1,32 @@
+import Walker from './Walker'
+
+// TODO UI for customizable parameters
+// TODO Collisions
+
+function sketch(p) {
+
+  var walkers = [];
+
+  p.setup = function () {
+    var canvas = p.createCanvas(p.windowWidth, p.windowHeight);
+    canvas.parent("sketch");
+
+    p.background(0);
+
+    for (var i = 0; i < 15; i++) {
+      walkers.push(new Walker(p));
+    }
+  };
+
+  p.draw = function () {
+    p.background(0, 10);
+
+    for (var i = 0; i < walkers.length; i++) {
+      walkers[i].draw();
+      walkers[i].update();
+    }
+  };
+
+}
+
+export default sketch
---
+++ react-ui/src/crafts/Perlin Walks/Walker.js
@@ -0,0 +1,23 @@
+export default class Walker {
+  constructor(p) {
+    this.p = p;
+    this.t = 0;
+    this.seed = p.random() * 1000;
+  }
+  draw() {
+    var p = this.p;
+    p.noiseSeed(this.seed);
+    var x = p.width * p.noise(this.t);
+    var y = p.height * p.noise(this.t+5);
+    var r = 255 * p.noise(this.t+10);
+    var g = 255 * p.noise(this.t+15);
+    var b = 255 * p.noise(this.t+20);
+
+    p.noStroke();
+    p.fill(r, g, b);
+    p.ellipse(x, y, 75, 75);
+  }
+  update() {
+    this.t += this.p.random() * 0.008;
+  }
+}
