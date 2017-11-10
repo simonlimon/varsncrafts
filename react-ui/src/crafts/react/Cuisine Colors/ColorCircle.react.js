57bae8079a3c630aff457fe7e56924eff8683b9f
simonlimon <simon.posada29@gmail.com>
2017-11-10 12:34:21 +0100

    Displaying images on hover

--- react-ui/src/crafts/react/Cuisine Colors/ColorCircle.react.js
+++ react-ui/src/crafts/react/Cuisine Colors/ColorCircle.react.js
@@ -3,21 +3,46 @@ import React from 'react'
 //TODO Add option to go to wikipedia articles
 
 class ColorCircle extends React.Component {
+  showImage = () => {
+    this.setState({showImage: true})
+  }
+
+  hideImage = () => {
+    this.setState({showImage: false})    
+  }
+
   render() {
     return (
-      <a
-        href={this.props.href}
-        style={{
-          backgroundColor: this.props.color.css(),
-          display: 'block',
-          width: this.props.radius,
-          height: this.props.radius,
-          borderRadius: '50%',
-          position: 'absolute',
-          left: this.props.x,
-          top: this.props.y
-        }}
-      />
+      <div>
+        <a
+          onMouseEnter={this.showImage}
+          onMouseLeave={this.hideImage}
+          href={this.props.href}
+          style={{
+            backgroundColor: this.props.color.css(),
+            display: 'block',
+            width: this.props.radius,
+            height: this.props.radius,
+            borderRadius: '50%',
+            position: 'absolute',
+            left: this.props.x,
+            top: this.props.y
+          }}
+        />
+        {this.state && this.state.showImage && 
+          <img 
+            src={this.props.href} 
+            style={{
+              width: this.props.radius * 3,
+              height: this.props.radius * 3,
+              borderRadius: '50%',
+              position: 'absolute',
+              left: window.innerWidth/2 - this.props.radius * 3/2,
+              top: window.innerHeight/2 - this.props.radius * 3/2
+            }}
+          />
+        }
+      </div>
     );
   }
 
