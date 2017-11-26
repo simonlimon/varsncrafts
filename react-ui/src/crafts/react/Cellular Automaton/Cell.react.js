79095f8579d9103862f50f5b5148d74cf8171f33
hbendekgey <henry.bendekgey@pomona.edu>
2017-11-26 17:06:54 +0100

    Added generation counter and rule space

--- react-ui/src/crafts/react/Cellular Automaton/Cell.react.js
+++ react-ui/src/crafts/react/Cellular Automaton/Cell.react.js
@@ -10,7 +10,7 @@ class Cell extends React.Component {
         style={{
           position: 'absolute',
           left: this.props.size * this.props.row,
-          top: this.props.size * this.props.col + window.innerHeight / 10,
+          top: this.props.size * this.props.col + 75,
           width: this.props.size,
           height: this.props.size,
           backgroundColor: this.props.alive && "#000000"}} />
--- react-ui/src/crafts/react/Cellular Automaton/Main.react.js
+++ react-ui/src/crafts/react/Cellular Automaton/Main.react.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Cell from './Cell.react';
-import { Button } from 'semantic-ui-react';
+import { Button, Label } from 'semantic-ui-react';
 
-// TODO better resize (don't clear everything)
-// TODO implement dropdown menu for settings
-// TODO implement generation #
-// TODO fix performance (more)
+// TODO make top bar prettier
+// TODO add description and image
+// TODO deal with warnings for maps
+
 
 class Main extends React.Component {
   constructor(props) {
@@ -14,24 +14,36 @@ class Main extends React.Component {
       cols: 0,
       rows: 0,
       cellMatrix: [],
-      isRunning: false
+      survivalRules: [],
+      birthRules: [],
+      isRunning: false,
+      generation: 0,
     };
   }
 
   updateDimensions = () => {
     let cellMatrix = [];
     const cols = window.innerWidth / 20;
-    const rows = window.innerHeight / 20;
+    const rows = (window.innerHeight - 75) / 20;
     for (let i = 0; i < cols; i++) {
       cellMatrix.push([]);
+      let wasCol = this.state.cellMatrix[i];
       for (let j = 0; j < rows; j++) {
-        cellMatrix[i].push({alive: false, neighbors: 0});
+        if (wasCol && this.state.cellMatrix[i][j]) {
+          cellMatrix[i].push(this.state.cellMatrix[i][j]);
+        } else {
+          cellMatrix[i].push({alive: false, neighbors: 0});
+        }
       }
     }
     this.setState({rows: rows, cols: cols, cellMatrix: cellMatrix});
   }
 
   componentWillMount() {
+    this.setState({
+      birthRules: [false, false, false, true, false, false, false, false, false],
+      survivalRules: [false, false, true, true, false, false, false, false, false]
+    })
     this.updateDimensions(this);
   }
 
@@ -57,21 +69,26 @@ class Main extends React.Component {
 
   evolve() {
     this.state.cellMatrix.map((row, i) => {
-        row.map((cell, j) => {
-          cell.neighbors = this.countNeighbors(i,j);
-        })
-      });
-      this.state.cellMatrix.map((row, i) => {
-        row.map((cell, j) => {
-          if (cell.alive && (cell.neighbors < 2 || cell.neighbors > 3)) {
-            cell.alive = false;
-          } else if (!cell.alive && cell.neighbors === 3) {
-            cell.alive = true;
-          }
-        })
-        });
-      this.forceUpdate();
-    }
+      row.map((cell, j) => {
+        cell.neighbors = this.countNeighbors(i,j);
+      })
+    });
+
+    this.state.cellMatrix.map((row, i) => {
+      row.map((cell, j) => {
+        if (cell.alive && !this.state.survivalRules[cell.neighbors]) {
+          cell.alive = false;
+        } else if (!cell.alive && this.state.birthRules[cell.neighbors]) {
+          cell.alive = true;
+        }
+      })
+    });
+
+    this.setState(prevState => {
+      prevState.generation += 1;
+      return {generation: prevState.generation};
+    });
+  }
 
   render() {
     return (
@@ -82,21 +99,21 @@ class Main extends React.Component {
             left: 0,
             top: 0,
             width: window.innerWidth,
-            height: window.innerHeight / 10,
+            height: 75,
             backgroundColor: "#D3D3D3"}}
         >
         <Button
-        style={{
-          position: 'absolute',
-          left: 100,
-          top: 20,
-        }}
+          style={{
+            position: 'absolute',
+            left: 100,
+            top: 20,
+          }}
           size='large'
-          icon={this.state.isRunning && "stop" || "play"}
+          icon={(this.state.isRunning && "stop") || "play"}
           className="play_button"
           onClick={() => {
             if (!this.state.isRunning) {
-              this.setState({isRunning: setInterval(function(e) {e.evolve(); }, 150, this)});
+              this.setState({isRunning: setInterval(function(e) {e.evolve(); }, 100, this)});
             } else {
               clearInterval(this.state.isRunning);
               this.setState({isRunning: null});
@@ -104,22 +121,22 @@ class Main extends React.Component {
           }}
         />
         <Button
-        style={{
-          position: 'absolute',
-          left: 150,
-          top: 20,
-        }}
+          style={{
+            position: 'absolute',
+            left: 150,
+            top: 20,
+          }}
           size='large'
           icon='step forward'
           className="step_button"
           onClick={() => {!this.state.isRunning && this.evolve();}}
         />
         <Button
-        style={{
-          position: 'absolute',
-          left: 200,
-          top: 20,
-        }}
+          style={{
+            position: 'absolute',
+            left: 200,
+            top: 20,
+          }}
           size='large'
           icon='erase'
           className="clear_button"
@@ -129,15 +146,15 @@ class Main extends React.Component {
                 cell.alive = false;
               })
             });
-            this.forceUpdate();
+            this.setState({generation: 0});
           }}
         />
         <Button
-        style={{
-          position: 'absolute',
-          left: 250,
-          top: 20,
-        }}
+          style={{
+            position: 'absolute',
+            left: 250,
+            top: 20,
+          }}
           size='large'
           icon='random'
           className="random_button"
@@ -147,9 +164,54 @@ class Main extends React.Component {
                 cell.alive = Math.random() > 0.8;
               })
             });
-            this.forceUpdate();
+            this.setState({generation: 0});
           }}
         />
+        <Label
+          style={{
+            position: 'absolute',
+            left: 300,
+            top: 24,
+          }}
+          content={'Generation: ' + this.state.generation}
+        />
+        <Button.Group
+          style={{
+            position: 'absolute',
+            left: 400,
+            top: 20,
+          }}
+        >
+          {this.state.birthRules.map((rule, i) =>
+            <Button
+              color={(rule && 'green') || 'red'}
+              content={i}
+              key={i}
+              onClick={() => {this.setState(prevState => {
+                prevState.birthRules[i] = !rule;
+                return {birthRules: prevState.birthRules};
+              })}} />
+          )}
+        </ Button.Group>
+        <Button.Group
+          style={{
+            position: 'absolute',
+            left: 880,
+            top: 20,
+          }}
+        >
+          {this.state.survivalRules.map((rule, i) =>
+            <Button
+              color={(rule && 'green') || 'red'}
+              content={i}
+              key={i}
+              onClick={() => {this.setState(prevState => {
+                prevState.survivalRules[i] = !rule;
+                return {survivalRules: prevState.survivalRules};
+              })}}
+            />
+          )}
+        </ Button.Group>
         </div>
         {this.state.cellMatrix.map((row, i) =>
           this.state.cellMatrix[i].map((cell, j) =>
